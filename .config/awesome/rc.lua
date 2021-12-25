782b5b9948711177f6f4d44e52e9a6e9df621eab
angeD <ange.duhayon@epitech.eu>
2021-12-25 22:18:09 +0100

    feat: removed mouse shortcuts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -103,30 +103,6 @@ local taglist_buttons = gears.table.join(
                 client.focus:move_to_tag(t)
             end
         end
-    ),
-    awful.button(
-        {}, 3,
-        awful.tag.viewtoggle
-    ),
-    awful.button(
-        {modkey}, 3,
-        function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ),
-    awful.button(
-        {}, 4,
-        function(t)
-            awful.tag.viewnext(t.screen)
-        end
-    ),
-    awful.button(
-        {}, 5,
-        function(t)
-            awful.tag.viewprev(t.screen)
-        end
     )
 )
 
@@ -139,24 +115,6 @@ local tasklist_buttons = gears.table.join(
             else
                 c:emit_signal("request::activate", "tasklist", {raise = true})
             end
-        end
-    ),
-    awful.button(
-        {}, 3,
-        function()
-            awful.menu.client_list({theme = {width = 250}})
-        end
-    ),
-    awful.button(
-        {}, 4,
-        function()
-            awful.client.focus.byidx(1)
-        end
-    ),
-    awful.button(
-        {}, 5,
-        function()
-            awful.client.focus.byidx(-1)
         end
     )
 )
