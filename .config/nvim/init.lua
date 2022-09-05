3853d5bc51ebefd925ebb36dc4503ed14d96271a
AngeD <aduhayon@predicloud.com>
2022-09-05 17:06:14 +0200

    feat: remove unused movements shortcuts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,12 +46,6 @@ vim.wo.cc            = "80"
 -- terminal escape key
 vim.keymap.set("t",  "<Esc>",     "<C-\\><C-n>"       )
 
--- movement keys
-vim.keymap.set("n",  "<C-h>",     "<C-w>h"            )
-vim.keymap.set("n",  "<C-j>",     "<C-w>j"            )
-vim.keymap.set("n",  "<C-k>",     "<C-w>k"            )
-vim.keymap.set("n",  "<C-l>",     "<C-w>l"            )
-
 -- go to file:line instead of just file
 vim.keymap.set("n",  "gf",        "gF"                )
 
