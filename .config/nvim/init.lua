1938b20c822283b2b5c7cafd116dede2405582af
AngeD <ange.duhayon@epitech.eu>
2023-04-09 12:49:01 +0200

    feat: nvim cin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,43 +2,48 @@
 ----- variables -----
 ---------------------
 
-vim.g.mapleader      = " "
+vim.g.mapleader        = " "
 
-vim.o.expandtab      = true
-vim.o.smartindent    = true
-vim.o.shiftwidth     = 4
-vim.o.tabstop        = 4
+vim.o.expandtab        = true
+vim.o.smartindent      = true
+vim.o.shiftwidth       = 4
+vim.o.tabstop          = 4
+vim.o.cino             = "(s"
+vim.g.python_indent    = {
+    open_paren = "shiftwidth()"
+}
 
-vim.o.list           = true
-vim.o.listchars      = "tab:> ,trail:-,nbsp:+,leadmultispace:│   "
+vim.o.list             = true
+vim.o.listchars        = "tab:> ,trail:-,nbsp:+,leadmultispace:│   "
 
-vim.o.number         = true
-vim.o.relativenumber = true
-vim.o.laststatus     = 3
+vim.o.number           = true
+vim.o.relativenumber   = true
+vim.o.laststatus       = 3
 
-vim.o.ignorecase     = true
-vim.o.smartcase      = true
+vim.o.ignorecase       = true
+vim.o.smartcase        = true
 
-vim.o.guicursor      = ""
+vim.o.guicursor        = ""
 
-vim.o.path           = vim.o.path .. ",**"
-vim.o.suffixes       = vim.o.suffixes .. ",.pyc,.hi"
+vim.o.path             = vim.o.path .. ",**"
+vim.o.suffixes         = vim.o.suffixes .. ",.pyc,.hi"
 
-vim.o.foldmethod     = "indent"
-vim.o.foldlevel      = 99
+vim.o.foldmethod       = "indent"
+vim.o.foldlevel        = 99
 
-vim.o.grepprg        = "grep -rn"
+vim.o.grepprg          = "grep -rn"
 
-vim.o.scrolloff      = 2
-vim.wo.colorcolumn   = "80"
+vim.o.scrolloff        = 2
+vim.wo.colorcolumn     = "80"
+vim.g.black_linelength = 79
 
-vim.g.netrw_banner   = 0
+vim.g.netrw_banner     = 0
 
-vim.o.lazyredraw     = true
+vim.o.lazyredraw       = true
 
-vim.o.complete       = vim.o.complete .. ",i"
+vim.o.complete         = vim.o.complete .. ",i"
 
-vim.o.mousemodel     = extend
+vim.o.mousemodel       = extend
 
 --------------------
 ----- packages -----
